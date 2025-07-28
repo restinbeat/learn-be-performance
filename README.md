a334818590f4f20f38ba524f42af9b80868d8910
restinbeat <thek226@gmail.com>
2025-07-28 15:02:48 +0900

    docs: update readme.md(artillery)

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # learn-be-performance
 
 - 백엔드 성능 개선
+
+### install artillery
+```bash
+$ npm install -g artillery@latest
+```
--- src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlRestController.java
+++ src/main/java/kr/co/shortenurlservice/presentation/ShortenUrlRestController.java
@@ -1,16 +1,18 @@
 package kr.co.shortenurlservice.presentation;
 
+import jakarta.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import kr.co.shortenurlservice.application.SimpleShortenUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShortenUrlRestController {
