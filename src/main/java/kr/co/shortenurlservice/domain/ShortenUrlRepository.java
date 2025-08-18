a31489e49a645df94d9a43ceda4d9e56e8acf2df
restinbeat <thek226@gmail.com>
2025-08-18 10:14:37 +0900

    feat: ConcurrentHashMap(cache)

--- src/main/java/kr/co/shortenurlservice/domain/ShortenUrlRepository.java
+++ src/main/java/kr/co/shortenurlservice/domain/ShortenUrlRepository.java
@@ -3,5 +3,6 @@ package kr.co.shortenurlservice.domain;
 public interface ShortenUrlRepository {
     void saveShortenUrl(ShortenUrl shortenUrl);
     void asyncSaveShortenUrl(ShortenUrl shortenUrl);
+    void increaseRedirectCount(ShortenUrl shortenUrl);
     ShortenUrl findShortenUrlByShortenUrlKey(String shortenUrlKey);
 }
--- src/main/java/kr/co/shortenurlservice/infrastructure/JpaShortenUrlRepository.java
+++ src/main/java/kr/co/shortenurlservice/infrastructure/JpaShortenUrlRepository.java
@@ -2,7 +2,15 @@ package kr.co.shortenurlservice.infrastructure;
 
 import kr.co.shortenurlservice.domain.ShortenUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface JpaShortenUrlRepository extends JpaRepository<ShortenUrl, Long> {
     ShortenUrl findByShortenUrlKey(String shortenUrlKey);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE shorten_url SET shorten_url.redirectCount = shorten_url.redirectCount + 1 WHERE shorten_url.shortenUrlKey = :shortenUrlKey")
+    int incrementRedirectCount(String shortenUrlKey);
 }
